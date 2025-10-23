0b7a60cda3782c6bd86a8c946b65c4922c1ddd73
diego69-foda <diego.bkuhn@gmail.com>
2025-10-23 17:12:21 -0300

    fix: correct useState syntax in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export const App: React.FC = () => (
+  {const [key, setkey = React.useState;}
+
   <div className="App">
     <p className="App__message">The last pressed key is [Enter]</p>
   </div>
